bf8d82435857b64dc4dc1d902de8a592334a5b52
Markus Damm <nullachtfuffzn@arcor.de>
2015-11-06 16:43:22 +0100

    sort locations

--- app/src/main/java/de/markusdamm/pokerapp/LocationManagement.java
+++ app/src/main/java/de/markusdamm/pokerapp/LocationManagement.java
@@ -5,12 +5,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -19,14 +16,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 import de.markusdamm.pokerapp.data.Location;
-import de.markusdamm.pokerapp.data.Player;
 
 
 public class LocationManagement extends ActionBarActivity {
     private SQLiteDatabase database;
     private ListView locations;
-    private Button btn;
     private EditText et;
+    ArrayList<String> locationList = new ArrayList<>();
 
 
 
@@ -35,46 +31,49 @@ public class LocationManagement extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_management);
         fillList();
-        btn = (Button) findViewById(R.id.saveLocation);
         et = (EditText) findViewById(R.id.newLocation);
         addListenerOnListViewItemSelection();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == 7){
+            fillList();
+        }
+    }
 
-    public ArrayList<Location> getLocations(){
+
+    public void fillLocationList(){
+        locationList.clear();
         database = openOrCreateDatabase("pokerDB", MODE_PRIVATE,null);
-        ArrayList<Location> ret = new ArrayList<>();
 
-        Cursor cursor  = database.rawQuery("SELECT name FROM locations", null);
+        Cursor cursor  = database.rawQuery("SELECT name FROM locations ORDER BY name ASC", null);
         while(cursor.moveToNext()){
-            Location entry = new Location(cursor.getString(cursor.getColumnIndex("name")));
-            ret.add(entry);
+            String entry = cursor.getString(cursor.getColumnIndex("name"));
+            locationList.add(entry);
         }
         database.close();
-        return ret;
     }
 
     public void saveLocation(View view){
         String s = et.getText().toString();
         if (!s.equals("")){
-            Location location = new Location(s);
-            addLocationToDB(location);
+            addLocationToDB(s);
         }
         et.setText("");
     }
 
     public void fillList(){
-        ArrayList<Location> locationList = getLocations();
+        fillLocationList();
         locations = (ListView) findViewById(R.id.locations);
         ListAdapter listenAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList);
         locations.setAdapter(listenAdapter);
     }
 
-    public void addLocationToDB(Location location){
-        ArrayList<Location> oldLocations = getLocations();
-        if (!oldLocations.contains(location)){
+    public void addLocationToDB(String location){
+        if (!locationList.contains(location)){
             database = openOrCreateDatabase("pokerDB", MODE_PRIVATE,null);
-            database.execSQL("INSERT INTO locations (name) VALUES('" + location.getName() + "');'");
+            database.execSQL("INSERT INTO locations (name) VALUES('" + location + "');'");
             database.close();
             fillList();
         }
@@ -90,12 +89,24 @@ public class LocationManagement extends ActionBarActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Location item = (Location) locations.getItemAtPosition(position);
+                String item = (String) locations.getItemAtPosition(position);
+                Location location = new Location(item);
                 Intent intent = new Intent(parent.getContext(), SingleLocation.class);
-                //intent.putExtra("id",item.getId());
-                intent.putExtra("id", 1);
+                intent.putExtra("id", getIDForLocation(location));
                 startActivityForResult(intent, 0);
             }
         });
+    }
+
+    public int getIDForLocation(Location location){
+        database = openOrCreateDatabase("pokerDB", MODE_PRIVATE,null);
+
+        Cursor cursor  = database.rawQuery("SELECT id FROM locations WHERE name = '" + location.getName() + "';", null);
+        cursor.moveToLast();
+        int entry = cursor.getInt(cursor.getColumnIndex("id"));
+        cursor.close();
+        database.close();
+
+        return entry;
     }
 }
